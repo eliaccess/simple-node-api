e316e831f3bd477963ed284f23ad530e6517d170
Elias Limouni <elias.limouni@etu.utc.fr>
2024-10-18 21:01:46 +0200

    pushing an update to see effect on ci cd

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/test', (req, res) => {
-  res.send('Hello from test route!');
+  res.send('Hello from test route! This is the v2.');
+});
+
+app.get('/new', (req, res) => {
+  res.send('This is a new route.');
 });
 
 app.listen(port, () => {
